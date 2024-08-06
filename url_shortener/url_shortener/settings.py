5670da2c624c55c8e10f010223c0846428d62355
William Krasnov <167891083+jmac-willko@users.noreply.github.com>
2024-08-06 11:09:50 -0400

    favicons

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -110,7 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Security settings
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
